d8103f91dbcdfd3b4689e061bfd3e1ac149594b5
Ali Mehdi <alimehdi.official@gmail.com>
2026-07-05 12:57:10 +0500

    removed H5P main branch in order to avoid override

--- entrypoint.cron.sh
+++ entrypoint.cron.sh
@@ -4,16 +4,16 @@ php /var/www/html/artisan config:cache
 if [[ ! -e /var/www/html/storage/oauth-private.key || ! -e /var/www/html/storage/oauth-public.key ]]; then php /var/www/html/artisan passport:install; fi
 
 #New Relilc
-nr_enabled=$(printenv ENABLE_NR);
-if [ "$nr_enabled" -eq "1" ]; then
-  export NR_INSTALL_SILENT=true
-  export NR_INSTALL_KEY=$(printenv NR_INSTALL_KEY)
-  export nr_name=$(printenv NR_NAME)
-  sh /var/www/html/newrelic/newrelic-install install
-  sed -i -e "s/newrelic.appname =.*/newrelic.appname = \"\Curriki-API-Cron $nr_name\"/" /usr/local/etc/php/conf.d/newrelic.ini
-fi
+# nr_enabled=$(printenv ENABLE_NR);
+# if [ "$nr_enabled" -eq "1" ]; then
+#   export NR_INSTALL_SILENT=true
+#   export NR_INSTALL_KEY=$(printenv NR_INSTALL_KEY)
+#   export nr_name=$(printenv NR_NAME)
+#   sh /var/www/html/newrelic/newrelic-install install
+#   sed -i -e "s/newrelic.appname =.*/newrelic.appname = \"\Curriki-API-Cron $nr_name\"/" /usr/local/etc/php/conf.d/newrelic.ini
+# fi
 
-cp -rf /var/www/html/h5pcode/* /var/www/html/storage/app/public/h5p/
+# cp -rf /var/www/html/h5pcode/* /var/www/html/storage/app/public/h5p/
 chmod 777 -R /var/www/html/storage &
 
 service cron start &
